75d8824d83c016268ba1d28e9e12b916e93e145b
faridulhaque <faridulhaquemurshed@gmail.com>
2022-09-01 01:14:49 +0600

    done

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const client = new MongoClient(uri, {
 const transporter = nodemailer.createTransport({
   service: "SendinBlue",
   auth: {
-    user: process.env.SENDER_EMAIL,
-    pass: process.env.SMTP_PASSWORD,
+    user: 'arifulislam64743@gmail.com',
+    pass: 'aUOQPvSNRm26WXtC',
   },
 });
 function sendMailToMember(newMember) {
